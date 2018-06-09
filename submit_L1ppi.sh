c1832e8535046c83046bff2c603280ac52cb0b8d
David V. Smith <david.victor.smith@gmail.com>
2018-06-09 11:16:08 -0400

    added loops

--- submit_L1ppi.sh
+++ submit_L1ppi.sh
@@ -4,16 +4,20 @@
 for subj in 100408 100206 100307; do
   for RUN in LR RL; do
     for H in R L; do
+      for PPItype in full partial; do
+        for PPIseed in V1 OFA FFA ATL pSTS IFG AMG OFC PCC; do
 
-      #Manages the number of jobs and cores
-      SCRIPTNAME=L1ppi.sh
-      NCORES=24
-      while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
-        sleep 1m
+          #Manages the number of jobs and cores
+          SCRIPTNAME=L1ppi.sh
+          NCORES=24
+          while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
+            sleep 1m
+          done
+          bash $SCRIPTNAME $RUN $subj $H $PPIseed $PPItype &
+          sleep 5s
+
+        done
       done
-      bash $SCRIPTNAME $RUN $subj $H &
-      sleep 5s
-
     done
   done
 done
